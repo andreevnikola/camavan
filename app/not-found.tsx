2514968279f5915de7a686f01f6091a03707ad57
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-04 12:01:15 +0300

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="w-full h-[calc(100vh-100px)] flex justify-center items-center">
+      <h1>404 - Нямаме такава страница</h1>
+    </div>
+  );
+}
