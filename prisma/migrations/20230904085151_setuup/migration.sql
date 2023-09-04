901d1f2c57ddd42f3c107e08c085138dbac918e5
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-04 11:53:43 +0300

    db

---
+++ prisma/migrations/20230904085151_setuup/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('PARENTS', 'CHILDREN', 'GRUICHO', 'TEENS', 'UNKNOWN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "isBanned" BOOLEAN NOT NULL DEFAULT false,
+    "image_url" TEXT NOT NULL,
+    "category" "Category" NOT NULL DEFAULT 'UNKNOWN',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_image_url_key" ON "User"("image_url");
