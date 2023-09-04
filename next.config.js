1f94c7104d013acea28a6552cd79262c0c16067b
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-04 21:40:35 +0300

    added scss

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 module.exports = nextConfig;
