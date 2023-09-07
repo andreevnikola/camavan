3a6d693c72202cfd7cb795211cd94ea907d288e4
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-07 17:30:26 +0300

    idk

--- next.config.js
+++ next.config.js
@@ -12,6 +12,17 @@ const nextConfig = {
           key: "Cache-Control",
           value: "no-store",
         },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,DELETE,PATCH,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
       ],
     },
   ],
