c49bdff824c1a959ad1439856490734e65ac9104
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-07 17:48:13 +0300

    yesss

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -3,15 +3,14 @@ import { CreateNewEvent } from "@/components/NewEvent";
 import { currentUser } from "@clerk/nextjs";
 
 export default async function NewEvent() {
-  const clerkUser = (await currentUser()) || undefined;
-  const user = clerkUser
-    ? await db.user.findUnique({
-        where: {
-          id: clerkUser?.id,
-        },
-      })
-    : undefined;
-
+  const clerkUser = await currentUser();
+  console.log(clerkUser);
+  const user = await db.user.findUnique({
+    where: {
+      id: clerkUser?.id,
+    },
+  });
+  console.log(user);
   if (user?.hasRole === "ADMIN") {
     return <CreateNewEvent />;
   } else {
