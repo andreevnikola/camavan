4908276d36618954940387959da182948ac3e934
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-09-07 16:23:33 +0300

    merge fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,45 +14,35 @@ import { useState } from "react";
 
 export function Header() {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
-  return (<>
-    <header id="header">
-      <div className="desktop-navigation-menu">
-        <div className='nav_left_links'>
-          <Image className="logo" src="/images/logo.jpg" alt="logo" width={1200} height={1200} />
-          <div className="nav_links">
-            <Link className="nav_links_link" href="/">Начало</Link>
-            <Link className="nav_links_link" href="/about-us">За нас</Link>
-            <Link className="nav_links_link" href="/workshops">Работилници</Link>
-            <Link className="nav_links_link" href="#">Събития</Link>
+  return (
+    <>
+      <header id="header">
+        <div className="desktop-navigation-menu">
+          <div className="nav_left_links">
+            <Image
+              className="logo"
+              src="/images/logo.jpg"
+              alt="logo"
+              width={1200}
+              height={1200}
+            />
+            <div className="nav_links">
+              <Link className="nav_links_link" href="/">
+                Начало
+              </Link>
+              <Link className="nav_links_link" href="/about-us">
+                За нас
+              </Link>
+              <Link className="nav_links_link" href="/workshops">
+                Работилници
+              </Link>
+              <Link className="nav_links_link" href="/events">
+                Събития
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="nav_register_links">
-          <SignedOut>
-            <SignInButton afterSignInUrl="/" afterSignUpUrl="/" mode="modal">
-              <a className="btn max-md:text-xs">Влез</a>
-            </SignInButton>
-            <SignUpButton afterSignInUrl="/" afterSignUpUrl="/" mode="modal">
-              <a className="btn btn-primary max-md:text-xs">Регистрирай се</a>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton afterSignOutUrl="/" />
-          </SignedIn>
-        </div>
-      </div>
-      <div className="mobile-navigation-menu">
-        <Image className="logo" src="/images/logo.jpg" alt="logo" width={1200} height={1200} />
-        <a id="btn-hamburger" className={`hamburger-menu ${isHamburgerOpen ? "open" : ""}`} onClick={() => { setIsHamburgerOpen(!isHamburgerOpen)}}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-        <div className={`mobile-nav-links ${isHamburgerOpen ? "mobile-nav-links-opened" : ""}`}>
-          <Link className="nav_links_link" href="/">Начало</Link>
-          <Link className="nav_links_link" href="/about-us">За нас</Link>
-          <Link className="nav_links_link" href="/workshops">Работилници</Link>
-          <Link className="nav_links_link" href="#">Събития</Link>
-          <SignedOut>
+          <div className="nav_register_links">
+            <SignedOut>
               <SignInButton afterSignInUrl="/" afterSignUpUrl="/" mode="modal">
                 <a className="btn max-md:text-xs">Влез</a>
               </SignInButton>
@@ -63,9 +53,58 @@ export function Header() {
             <SignedIn>
               <UserButton afterSignOutUrl="/" />
             </SignedIn>
+          </div>
         </div>
-      </div>
-    </header>
+        <div className="mobile-navigation-menu">
+          <Image
+            className="logo"
+            src="/images/logo.jpg"
+            alt="logo"
+            width={1200}
+            height={1200}
+          />
+          <a
+            id="btn-hamburger"
+            className={`hamburger-menu ${isHamburgerOpen ? "open" : ""}`}
+            onClick={() => {
+              setIsHamburgerOpen(!isHamburgerOpen);
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+          <div
+            className={`mobile-nav-links ${
+              isHamburgerOpen ? "mobile-nav-links-opened" : ""
+            }`}
+          >
+            <Link className="nav_links_link" href="/">
+              Начало
+            </Link>
+            <Link className="nav_links_link" href="/about-us">
+              За нас
+            </Link>
+            <Link className="nav_links_link" href="/workshops">
+              Работилници
+            </Link>
+            <Link className="nav_links_link" href="/events">
+              Събития
+            </Link>
+            <SignedOut>
+              <SignInButton afterSignInUrl="/" afterSignUpUrl="/" mode="modal">
+                <a className="btn max-md:text-xs">Влез</a>
+              </SignInButton>
+              <SignUpButton afterSignInUrl="/" afterSignUpUrl="/" mode="modal">
+                <a className="btn btn-primary max-md:text-xs">Регистрирай се</a>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
